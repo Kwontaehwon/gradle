9c4e73e38208722b15a52e7b32b11ecb5569964d
Kwontaehwon <kygkth2011@gmail.com>
2021-12-01 18:37:15 +0900

    first commit

---
+++ src/main/java/bullet.java
@@ -0,0 +1,21 @@
+public class bullet {
+    private int X;
+    private int Y;
+
+
+    public bullet(int x, int y){
+        this.X = x;
+        this.Y = y;
+    }
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("fds");
+    }
+}
---
+++ src/test/java/bulletTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class bulletTest {
+
+    bullet a;
+
+    @BeforeEach
+    void setUp() {
+        a = new bullet(1,2);
+    }
+
+    @Test
+    @DisplayName("Get X TEST")
+    void getX(){
+        assertEquals(1, a.getX());
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
